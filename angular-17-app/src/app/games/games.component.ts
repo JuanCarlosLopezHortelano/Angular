fef5fe363c7064697d6c7f2f96bf8b0dfa0bbea8
JuanCarlosLopezHortelano <juancaloslhhellin@gmail.com>
2025-02-16 19:45:01 +0100

    Comunicacion Bidireccional - Input - Output

--- angular-17-app/src/app/games/games.component.ts
+++ angular-17-app/src/app/games/games.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-games',
   imports: [],
   template: `
+    <h2>Username: {{ username }}</h2>
     <ul>
       @for(game of games; track game.id){
-        <li>{{ game.name }}</li>
+        <li (click)="fav(game.name)">{{ game.name }}</li>
       }
     </ul>
+    
   `,
   styles: ``
 })
 export class GamesComponent {
+  @Input() username = '';
+  @Output() addFavoriteEvent = new EventEmitter<string>();
+
+  fav(game: string) {
+    this.addFavoriteEvent.emit(game);
+  }
   games = [
     {
       id: 1,
