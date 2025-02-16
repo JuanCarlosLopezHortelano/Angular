1b152639027cb1ab57cbb4de99aedfb9b550e3eb
JuanCarlosLopezHortelano <juancaloslhhellin@gmail.com>
2025-02-16 19:13:31 +0100

    2 Componentes nuevos

---
+++ angular-17-app/src/app/games/games.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  imports: [],
+  template: `
+    <ul>
+      @for(game of games; track game.id){
+        <li>{{ game.name }}</li>
+      }
+    </ul>
+  `,
+  styles: ``
+})
+export class GamesComponent {
+  games = [
+    {
+      id: 1,
+      name: 'Super Mario Bros'
+    },
+    {
+      id: 2,
+      name: 'Zelda'
+    },
+    {
+      id: 3,
+      name: 'Donkey Kong'
+    }
+  ]
+}
